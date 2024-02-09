8c6bbcfcef7cf70be5201cab024aa0fc185072a2
hesamjafarian <hesamjafarian@gmail.com>
2024-02-09 22:51:16 +0200

    adding text as a main menu!

--- src/main.py
+++ src/main.py
@@ -2,35 +2,42 @@ import pygame
 import sys
 from const import *
 
-#Step 2
+# Step 2
 from game import Game
+
 
 # Game variables
 
+
 class Main:
     def __init__(self):
-        #print("Hi from init")
-        self.game_paused = False
+        # print("Hi from init")4
         pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
-        pygame.display.set_caption('Chess&AI')
-        #step 2
+        self.game_paused = False
+        self.font = pygame.font.SysFont("arialblack", 40)
+        self.text_color = (0, 0, 0)
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Chess&AI")
+        # step 2
         self.game = Game()
-    def _draw_pause_menu(self,):
-        pass
+
+    def _draw_text(self, text, x, y):
+        img = self.font.render(text, True, self.text_color)
+        self.screen.blit(img, (x, y))
+
     def mainloop(self):
-        #print("Hi from mainloop")
+        # print("Hi from mainloop")
         screen = self.screen
         game = self.game
         while True:
-            #step 2
+            # step 2
             game.show_bg(screen)
             game.show_pieces(screen)
 
             # Pause_Menu
             if self.game_paused == True:
-                print("Space key pressed!")
-            #step 1
+                self._draw_text("Main Menu", 280, 300)
+            # step 1
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
@@ -39,6 +46,7 @@ class Main:
                     pygame.quit()
                     sys.exit()
             pygame.display.update()
+
 
 main = Main()
 main.mainloop()
